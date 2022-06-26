e8f42a992e10105ab3012d3674f51b5bafe591eb
Cyrus Bigham <bighamcyrus@gmail.com>
2022-06-25 18:36:34 -0700

    api added to javascript

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,10 @@
 var Click = $("button")
 
+var cityInputEL = $("cityinput")
 
 
 
+cityInputEl = $(https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API key})
+console.log(textcontent)
 
 Click.on("click", enterEvent)
